e81756db7503a1758751b545d570ec710a32c022
lukas-wresch <93730321+lukas-wresch@users.noreply.github.com>
2021-11-06 20:39:15 +0100

    Add files via upload

---
+++ html/common.js
@@ -0,0 +1,511 @@
+var lang_en = {
+    name: "Name",
+    username: "Username",
+    password: "Password",
+    user: "User",
+    moderator: "Moderator",
+    admin: "Admin",
+    schedule: "Schedule",
+    match_tables: "Match Tables",
+    add_match: "Add Match",
+    add_matchtable: "Add Match Table",
+    update_matchtable: "Update Match Table",
+    delete_matchtable: "Delete Match Table",
+    matchtable: "Match Table",
+    matchtables: "Match Tables",
+    new_club: "New Club",
+    new_match: "New Match",
+    participants: "Participants",
+    database: "Database",
+    new_judoka: "New Judoka",
+    new_participant: "New Participant",
+    add_participant: "Add Participant",
+    list_judokas: "List Judokas",
+    list_of_all_clubs: "List of all Clubs",
+    list_of_all_judokas: "List of all Judokas",
+    new_account: "New Account",
+    list_accounts: "List Accounts",
+    rule_set: "Rule Set",
+    rule_sets: "Rule Sets",
+    edit_rule_set: "Edit Rule Set",
+    tournament: "Tournament",
+    tournaments: "Tournaments",
+    tournament_default: "Tournament Default",
+    overview: "Overview",
+    matches: "Matches",
+    next_matches: "Next Matches",
+    update_account: "Update Account",
+    update_club: "Update Club",
+    new_rule_set: "New Rule Set",
+    update_rule_set: "Update Rule Set",
+    new_tournament: "New Tournament",
+    update_tournament: "Update Tournament",
+    hide_mats: "Hide Mats",
+    show_mats: "Show Mats",
+    firstname: "Given name",
+    lastname: "Family name",
+    weight: "Weight",
+    gender: "Gender",
+    all: "All",
+    male: "Male",
+    female: "Female",
+    settings: "Settings",
+    type: "Type",
+    weightclass: "Weightclass",
+    min_weight: "Minimum weight",
+    max_weight: "Maximum weight",
+    details: "Details",
+    time: "Time",
+    for: "For",
+    mat: "Mat",
+    mats: "Mats",
+    neutral: "Neutral",
+    white: "White",
+    blue: "Blue",
+    draw: "Draw",
+    name_white: "Name (White)",
+    name_blue: "Name (Blue)",
+    status: "Status",
+    winner: "Winner",
+    score: "Score",
+    scheduled: "Scheduled",
+    running: "Running",
+    concluded: "Concluded",
+    match_time: "Match Time",
+    goldenscore_time: "Golden Score Time",
+    enabled:  "Enabled",
+    disabled: "Disabled",
+    osaekomi_time_ippon:   "Osaekomi Time (Ippon)",
+    osaekomi_time_wazaari: "Osaekomi Time (Waza-Ari)",
+    break_time: "Break Time",
+    start_next_match: "Start Next Match",
+    end_match: "End Match",
+    more: "More",
+    less: "Less",
+    remove: "Remove",
+    delete_match: "Delete Match",
+    delete_match_confirm: "Are you sure you want to delete this match?",
+    delete_matchtable_confirm: "Are you sure you want to delete this match table?",
+    delete_account_confirm: "Are you sure you want to delete this account?",
+    delete_judoka: "Delete",
+    delete_judoka_confirm: "Are you sure you want to delete this judoka?",
+    empty_tournament_confirm: "Are you sure you want to delete all match results of this tournament?",
+    delete_tournament_confirm: "Are you sure you want to completely delete this tournament?",
+    delete_rules_confirm: "Are you sure you want to delete this rule set?",
+    shutdown_confirm: "Are you sure you want to shutdown Judoboard?"
+}
+
+
+
+var lang_de = {
+    name: "Name",
+    username: "Benutzername",
+    password: "Passwort",
+    user: "Benutzer",
+    moderator: "Moderator",
+    admin: "Administrator",
+    schedule: "Zeitplan",
+    match_tables: "Kampflisten",
+    add_match: "Kampf hinzuf&uuml;gen",
+    add_matchtable: "Kampfliste hinzuf&uuml;gen",
+    update_matchtable: "Kampfliste &auml;ndern",
+    delete_matchtable: "Kampfliste l&ouml;schen",
+    matchtable: "Kampfliste",
+    matchtables: "Kampflisten",
+    new_club: "Neuer Verein",
+    new_match: "Neuer Kampf",
+    new_participant: "Neuer Teilnehmer",
+    add_participant: "Teilnehmer  hinzuf&uuml;gen",
+    participants: "Teilnehmer",
+    database: "Stammdaten",
+    new_judoka: "Neuer Judoka",
+    list_judokas: "Judokas anzeigen",
+    list_of_all_clubs: "Liste aller Vereine",
+    list_of_all_judokas: "Liste aller Judokas",
+    //new_account: "Neuer Account",
+    list_accounts: "Accounts anzeigen",
+    rule_set:  "Regelwerk",
+    rule_sets: "Regelwerke",
+    edit_rule_set: "Regelwerk &auml;ndern",
+    tournament: "Turnier",
+    tournaments: "Turniere",
+    tournament_default: "Turnierstandard",
+    overview: "&Uuml;bersicht",
+    matches: "K&auml;mpfe",
+    next_matches: "N&auml;chste K&auml;mpfe",
+    new_account: "Neues Benutzerkonto",
+    update_account: "Benutzerkonto &auml;ndern",
+    update_club: "Verein &auml;ndern",
+    new_rule_set: "Neues Regelwerk",
+    update_rule_set: "&Auml;ndere Regelwerk",
+    new_tournament: "Neues Turnier",
+    update_tournament: "&Auml;ndere Turnier",
+    hide_mats: "Matten ausblenden",
+    show_mats: "Matten anzeigen",
+    firstname: "Vorname",
+    lastname:  "Nachname",
+    weight: "Gewicht",
+    min_weight: "Mindestgewicht",
+    max_weight: "Maximalgewicht",
+    gender: "Geschlecht",
+    all: "Alle",
+    male: "M&auml;nnlich",
+    female: "Weiblich",
+    settings: "Einstellungen",
+    type: "Typ",
+    weightclass: "Gewichtsklasse",
+    details: "Details",
+    time: "Zeit",
+    for: "F&uuml;r",
+    mat: "Matte",
+    mats: "Matten",
+    neutral: "Neutral",
+    white: "Wei&szlig;",
+    draw: "Unentschieden",
+    blue: "Blau",
+    name_white: "Name (Wei&szlig;)",
+    name_blue: "Name (Blau)",
+    status: "Status",
+    winner: "Gewinner",
+    score: "Unterwertung",
+    scheduled: "Eingeplant",
+    running: "am K&auml;mpfen",
+    concluded: "Beendet",
+    match_time: "Kampfzeit",
+    goldenscore_time: "Golden Score Zeit",
+    enabled:  "Aktiviert",
+    disabled: "Deaktiviert",
+    osaekomi_time_ippon:   "Osaekomi Zeit (Ippon)",
+    osaekomi_time_wazaari: "Osaekomi Zeit (Waza-Ari)",
+    break_time: "Ruhepause",
+    start_next_match: "Kampf starten",
+    end_match: "Kampf beenden",
+    more: "Mehr",
+    less: "Weniger",
+    remove: "Entfernen",
+    delete_match: "L&ouml;sche Kampf",
+    delete_match_confirm: unescape("Willst du diesen Kampf wirklich l%F6schen?"),
+    delete_matchtable_confirm: unescape("Willst du diese Kampfliste wirklich l%F6schen?"),
+    delete_account_confirm: unescape("Willst du dieses Benutzerkonto wirklich l%F6schen?"),
+    delete_judoka: "L&ouml;schen",
+    delete_judoka_confirm: unescape("Willst du diesen Judoka wirklich l%F6schen?"),
+    empty_tournament_confirm: unescape("Willst du wirklich alle Ergebnisse dieses Turnieres l%F6schen?"),
+    delete_tournament_confirm: unescape("Willst du wirklich dieses Turnier vollst%E4ndig l%F6schen?"),
+    delete_rules_confirm: unescape("Willst du wirklich dieses Regelwerk l%F6schen?"),
+    shutdown_confirm: "Soll Judoboard wirklich heruntergefahren werden?"
+}
+
+
+
+var lang = lang_de;
+
+
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+
+
+function NiceTime(timestamp)
+{
+  return Math.floor(timestamp/(1000*60)) + ':' + zeroPad(Math.floor((timestamp/1000))%60, 2) + '.' + Math.floor((timestamp%1000)/100);
+}
+
+function NiceTimeMS(timestamp)
+{
+  return Math.floor(timestamp/(1000*60)) + ':' + zeroPad(Math.floor((timestamp/1000))%60, 2) + '.' + zeroPad(timestamp%1000, 3);
+}
+
+function NiceLargeTime(timestamp)
+{
+  return Math.floor(timestamp/(1000*60*60)) + ':' + zeroPad(Math.floor(timestamp/(1000*60)%60), 2) + ':' + zeroPad(Math.floor((timestamp/1000))%60, 2);
+}
+
+
+
+function URLEncode(obj)
+{
+  var str = [];
+  for (var p in obj)
+  {
+    if (obj.hasOwnProperty(p))
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+  }
+  return str.join("&");
+}
+
+
+
+function URLDecode(input, variable)
+{
+  if (typeof input === 'undefined')
+    return false;
+
+  var query = input.split("?");
+
+  if (typeof query[1] === 'undefined')
+    return false;
+
+  var vars = query[1].split("&");
+
+  for (var i=0;i < vars.length;i++)
+  {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable)
+      return pair[1];
+  }
+  return false;
+}
+
+
+
+function GetQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i < vars.length;i++)
+    {
+          var pair = vars[i].split("=");
+          if (pair[0] == variable)
+            return pair[1];
+    }
+    return false;
+}
+
+
+
+function Ajax(url)
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200)
+    {
+      if (this.response != "ok")
+        alert("Error: " + this.response);
+    }
+  };
+
+  xhttp.open("GET", url);
+  xhttp.send();
+  return false;
+}
+
+
+
+function AjaxCallback(url, callback)
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200 && typeof callback !== 'undefined')
+      callback(this.responseText);
+  };
+
+  xhttp.open("GET", url);
+  xhttp.send();
+}
+
+
+
+function AjaxPost(url, params, callback)
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", url);
+  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200)
+    {
+        if (this.response == "ok")
+          callback();
+        else
+          alert("Error: " + this.response);
+      }
+  };
+
+  xhttp.send(params);
+}
+
+
+
+function Color(index)
+{
+  if (index == 1)
+    return "#0bf8";
+  else if (index == 2)
+    return "#e338";
+  else if (index == 3)
+    return "#5d58";
+  else if (index == 4)
+    return "#ff68";
+  else if (index == 5)
+    return "#d0d8";
+  else if (index == 6)
+    return "#8658";
+  else if (index == 7)
+    return "#4ed8";
+  else if (index == 8)
+    return "#f858";
+  else if (index == 9)
+    return "#a0a8";
+  else if (index == 10)
+    return "#fe88";
+
+  return "#fff";
+}
+
+
+
+function navigate(url)
+{
+  window.clearInterval(TimerID);
+
+  window.location = '/#' + url;
+  URL = url;
+
+  if (window.innerWidth <= 900)//Mobile
+    slideout.close();
+
+  //$( "#main" ).load(url);
+  return false;
+}
+
+
+
+/*$(window).on('hashchange', function(e)
+{
+  console.log(location.hash.slice(1));
+  navigate(location.hash.slice(1));
+});*/
+
+window.onhashchange = function(e)
+{
+  console.log(location.hash.slice(1));
+  navigate(location.hash.slice(1));
+  $( "#main" ).load(URL);
+};
+
+
+var StreamTimer;
+var StreamUpdate = 0;
+
+function Stream()
+{
+  document.getElementById("img").src = "ajax/mat/screenshot?id=1&up=" + StreamUpdate;
+  StreamUpdate++;
+}
+
+
+
+function GetRuleSets(callback)
+{
+  AjaxCallback("ajax/rule/list", function(response) {
+    console.log(response);
+    var res = response.split(",");
+
+    if (res.length < 1)
+      return;
+
+    var rules = document.getElementById("rule");
+
+    while (rules.length >= 1)
+      rules.remove(rules.length-1);
+
+    var default_rule_set = res[0];
+
+    for (var i=1; i < res.length; i)
+    {
+      var option = document.createElement("option");
+
+      var id   = res[i++];
+      var name = res[i++];
+      var desc = res[i++];
+
+      option.value = id;
+
+      if (option.value == default_rule_set)
+        option.text = name + " (" + lang.tournament_default + ")";
+      else
+        option.text = name;
+
+      rules.add(option);
+    }
+
+    rules.value = default_rule_set;
+
+    if (typeof callback !== 'undefined')
+      callback();
+  });
+}
+
+
+
+function GetColors(callback)
+{
+  AjaxCallback("ajax/colors/get", function(response) {
+    var res = response.split(",");
+
+    if (res.length < 1)
+      return;
+
+    var color = document.getElementById("color");
+
+    while (color.length >= 1)
+      color.remove(color.length-1);
+
+    for (var i=0; i < res.length;)
+    {
+      var option = document.createElement("option");
+      option.value = res[i++];
+      option.text  = res[i++];
+      option.style.backgroundColor = res[i++].substring(0, 4);
+
+      color.add(option);
+    }
+
+    color.onchange = function(e)
+    {
+      for (var i=0; i < color.length;i++)
+      {
+        if (color.children[i].value == color.value)
+          color.style.backgroundColor = color.children[i].style.backgroundColor;
+      }
+    }
+
+
+    if (typeof callback !== 'undefined')
+      callback();
+  });
+}
+
+
+
+function GetMats(callback)
+{
+  AjaxCallback("ajax/config/get_mats", function(response) {
+    var res = response.split(",");
+
+    if (res.length < 2)
+      return;
+
+    var mat = document.getElementById("mat");
+    var maxID = res[0];
+
+    for (var i=1; i < res.length;)
+    {
+      var matID   = res[i++];
+      var type    = res[i++];
+      var is_open = res[i++];
+      var name    = res[i++];
+      var showIpponAsTwoWazaari = res[i++];
+
+      var option = document.createElement("option");
+      option.text  = name;
+      option.value = matID;
+      mat.add(option);
+    }
+
+    if (typeof callback !== 'undefined')
+      callback();
+  });
+}
