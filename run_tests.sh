905592349315e1716138e2059ccf5e0f828dfb53
Lukas Wresch <wresch@math.uni-bielefeld.de>
2021-11-09 07:34:35 +0100

    Fixed return code of unit tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,12 @@
 cd bin
 ./judoboard_GTest --gtest_output=xml
-cd ..
+if [ $? -eq 0 ]
+then
+  echo "All test ran successfull"
+  cd ..
+  exit 0
+else
+  echo "Unit tests failed"
+  cd ..
+  exit 1
+fi
