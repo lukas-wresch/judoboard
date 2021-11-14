4873d210ad7e3999a4d3e0b4958329d8679ab9c8
Lukas Wresch <wresch@math.uni-bielefeld.de>
2021-11-14 03:05:36 +0100

    Future proved ascii parsing

--- src/md5.cpp
+++ src/md5.cpp
@@ -1504,7 +1504,19 @@ std::string MD5::ReadLine(ZED::Blob& Data, bool* pStartOfHeading, bool* pNewLine
 			continue;
 		else if (c == 0x18)//Cancel
 			continue;
+		else if (c == 0x19)//End of Medium
+			continue;
+		else if (c == 0x1A)//Substitute
+			continue;
+		else if (c == 0x1B)//Escape
+			continue;
+		else if (c == 0x1C)//File Separator
+			continue;
 		else if (c == 0x1D)//Group Separator
+			continue;
+		else if (c == 0x1E)//Record Separator
+			continue;
+		else if (c == 0x1F)//Unit Separator
 			continue;
 		else if (c == '\r')
 		{
@@ -1540,7 +1552,7 @@ std::string MD5::ReadLine(ZED::Blob& Data, bool* pStartOfHeading, bool* pNewLine
 std::string MD5::RemoveControlCharacters(std::string& Str)
 {
 	std::string ret = Str;
-	char charactersToRemove[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0B, 0x0C, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x0E, 0x0F, 0x15, 0x1D, '\r', '\n'};
+	char charactersToRemove[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0B, 0x0C, 0x0F, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, '\r', '\n'};
 
 	for (auto c : charactersToRemove)
 		ret.erase(remove(ret.begin(), ret.end(), c), ret.end());
