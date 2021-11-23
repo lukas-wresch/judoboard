a964dbb850e606560c2da3b624be7aba7a4affc6
Lukas Wresch <wresch@math.uni-bielefeld.de>
2021-11-23 11:10:13 +0100

    tiny improvements2

--- compile.bat
+++ compile.bat
@@ -1,7 +1,6 @@
 cd vsprojects
 nuget install ZED\packages.config        -OutputDirectory packages
 nuget install GoogleTest\packages.config -OutputDirectory packages
-copy packages\libpng.1.6.28.1\build\native\lib\x64\v140\dynamic\Release\libpng16.lib ZED\libpng16.lib
 msbuild
 msbuild /p:Configuration=Release
 copy packages\libpng.redist.1.6.28.1\build\native\bin\x64\v140\dynamic\Debug\libpng16.dll   ..\bin\Debug
