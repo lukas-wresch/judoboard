b4af756e5cdfe6902d7435cc64bc8ccfe2c88bb1
Lukas Wresch <wresch@math.uni-bielefeld.de>
2022-01-30 22:05:11 +0100

    Added script to run unit tests with gdb attached under Linux.

---
+++ run_tests_debug.sh
@@ -0,0 +1,13 @@
+cd bin
+echo "Running tests with gdb attached"
+gdb -ex=r --args ./judoboard_test --gtest_output=xml
+if [ $? -eq 0 ]
+then
+  echo "All test ran successfull"
+  cd ..
+  exit 0
+else
+  echo "Unit tests failed"
+  cd ..
+  exit 1
+fi
