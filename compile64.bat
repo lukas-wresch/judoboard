ea3342f6159f37ea37f1ab46ae710f2fae84db58
Lukas Wresch <wresch@math.uni-bielefeld.de>
2022-08-18 15:01:58 +0200

    Removed compile.bat and renamed compile64.bat to compile.bat

--- compile64.bat
+++
@@ -1,9 +0,0 @@
-cd vsprojects
-nuget install ZED\packages.config        -OutputDirectory packages
-nuget install GoogleTest\packages.config -OutputDirectory packages
-msbuild
-msbuild /p:Configuration=Release
-copy packages\libpng.redist.1.6.28.1\build\native\bin\x64\v140\dynamic\Release\libpng16.dll ..\bin\libpng16.dll
-copy packages\zlib.v140.windesktop.msvcstl.dyn.rt-dyn.1.2.8.8\lib\native\v140\windesktop\msvcstl\dyn\rt-dyn\x64\Release\zlib.dll ..\bin\zlib.dll
-cd ..
-mkdir bin\tournaments
