10149494452bb18c974e69a2b6500d681547f854
Lukas Wresch <wresch@math.uni-bielefeld.de>
2022-11-20 16:21:39 +0100

    Changed mat_on_name UI from CSV to YAML. `GetNextMatches()` now returns the next 3 instead of 5 matches.

--- src/tournament.cpp
+++ src/tournament.cpp
@@ -706,7 +706,7 @@ std::vector<Match> Tournament::GetNextMatches(uint32_t MatID) const
 	Lock();
 
 	uint32_t id = 0;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		auto nextMatch = GetNextMatch(MatID, id);
 
