6c3150bbb2c264e6d350247b7545ea261837a1a2
Lukas Wresch <wresch@math.uni-bielefeld.de>
2022-11-28 03:38:23 +0100

    Added unit test for correct starting position with 16 participants.

--- tests/single_elimination.cpp
+++ tests/single_elimination.cpp
@@ -697,6 +697,23 @@ TEST(SingleElimination, Count16)
 	group.SetMatID(1);
 	t->AddMatchTable(&group);
 
+	EXPECT_TRUE(group.GetMatch(0)->Contains(*j[0]));
+	EXPECT_TRUE(group.GetMatch(0)->Contains(*j[8]));
+	EXPECT_TRUE(group.GetMatch(1)->Contains(*j[4]));
+	EXPECT_TRUE(group.GetMatch(1)->Contains(*j[12]));
+	EXPECT_TRUE(group.GetMatch(2)->Contains(*j[2]));
+	EXPECT_TRUE(group.GetMatch(2)->Contains(*j[10]));
+	EXPECT_TRUE(group.GetMatch(3)->Contains(*j[6]));
+	EXPECT_TRUE(group.GetMatch(3)->Contains(*j[14]));
+	EXPECT_TRUE(group.GetMatch(4)->Contains(*j[1]));
+	EXPECT_TRUE(group.GetMatch(4)->Contains(*j[9]));
+	EXPECT_TRUE(group.GetMatch(5)->Contains(*j[5]));
+	EXPECT_TRUE(group.GetMatch(5)->Contains(*j[13]));
+	EXPECT_TRUE(group.GetMatch(6)->Contains(*j[3]));
+	EXPECT_TRUE(group.GetMatch(6)->Contains(*j[11]));
+	EXPECT_TRUE(group.GetMatch(7)->Contains(*j[7]));
+	EXPECT_TRUE(group.GetMatch(7)->Contains(*j[15]));
+
 	ASSERT_EQ(group.GetParticipants().size(), 16);
 
 	for (auto match : group.GetSchedule())
